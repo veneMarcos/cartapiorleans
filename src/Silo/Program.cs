3a004610e1eec8e35830ff6005dfeb8c8eefbba9
Marcos Venezuela de Araujo <marcos.araujo@easynvest.com.br>
2020-07-31 18:40:43 -0300

    Adiciona grains

---
+++ src/Silo/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Logging;
+using Orleans;
+using Orleans.Hosting;
+using System;
+using System.Threading.Tasks;
+
+namespace Silo
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var siloBuilder = new SiloHostBuilder()
+                .UseLocalhostClustering(serviceId:"cartSilo")
+                .AddAdoNetGrainStorage("CartStorage", options =>
+                {
+                    options.Invariant = "System.Data.SqlClient";
+                    options.ConnectionString = "Data Source=localhost,1433;Initial Catalog=ServicePersistence;Integrated Security=False;User ID=sa;Password=P@55w0rd";
+                    options.UseJsonFormat = true;
+                })
+                .ConfigureLogging(logging => logging.AddConsole())
+                .UseDashboard();
+
+                using (var host = siloBuilder.Build())
+                {
+                    await host.StartAsync();
+                    Console.ReadLine();
+                }
+        }
+    }
+}
